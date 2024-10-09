1800c15430a20450d5e6b445fa1b6181a582ef11
Антон Маланьин <malanin.ao@phystech.edu>
2024-10-09 13:43:45 +0300

    add launching program with options

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
-int main() {
+#include "include/Automaton.h"
+
+int main(int argc, char** argv) {
+  Automaton automaton;
+  if (argv[1][1] == 's') {
+    automaton = create_from_reg_exp(argv[3]);
+  } else {
+    automaton = Automaton(static_cast<json>(argv[3]));
+  }
+  automaton.remove_epsilon_transitions();
+  if (argv[2][1] == 'd') {
+    automaton.make_determined();
+  } else if (argv[2][1] == 'm') {
+    automaton.make_minimal();
+  }
+  std::cout << static_cast<json>(automaton);
   return 0;
 }
